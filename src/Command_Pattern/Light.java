33e519f9f203c117aa0c5414392de4ea428b9a3e
in0w4k <in0w4.k@gmail.com>
2025-04-15 23:31:43 +0500

    Create light device

---
+++ src/Command_Pattern/Light.java
@@ -0,0 +1,11 @@
+package Command_Pattern;
+
+public class Light {
+    public void turnOn() {
+        System.out.println("[Light] Turning ON");
+    }
+
+    public void turnOff() {
+        System.out.println("[Light] Turning OFF");
+    }
+}
