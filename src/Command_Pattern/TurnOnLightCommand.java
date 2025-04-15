d8bdfbd746c9e8c4d01c5daad9b0c013668fea99
in0w4k <in0w4.k@gmail.com>
2025-04-15 23:32:02 +0500

    Create light command

---
+++ src/Command_Pattern/TurnOnLightCommand.java
@@ -0,0 +1,19 @@
+package Command_Pattern;
+
+public class TurnOnLightCommand implements Command {
+    private final Light light;
+
+    public TurnOnLightCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        light.turnOff();
+    }
+}
